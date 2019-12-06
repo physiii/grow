d042575f4aaf932c42fadc3e238dd0769c30e72b
Andy Payne <andy@pyfi.org>
2019-12-06 17:32:36 -0600

    starting code

---
+++ code/build/bootloader/soc/esp32/interrupts.d
@@ -0,0 +1,15 @@
+esp32/interrupts.o: \
+ /usr/local/src/esp/esp-idf/components/soc/esp32/interrupts.c \
+ /usr/local/src/esp/esp-idf/components/soc/include/soc/interrupts.h \
+ /usr/local/src/esp/esp-idf/components/soc/esp32/include/soc/soc.h \
+ /usr/local/src/esp/esp-idf/components/esp_common/include/esp_assert.h \
+ /usr/local/src/esp/esp-idf/components/esp_common/include/esp_bit_defs.h
+
+/usr/local/src/esp/esp-idf/components/soc/include/soc/interrupts.h:
+
+/usr/local/src/esp/esp-idf/components/soc/esp32/include/soc/soc.h:
+
+/usr/local/src/esp/esp-idf/components/esp_common/include/esp_assert.h:
+
+/usr/local/src/esp/esp-idf/components/esp_common/include/esp_bit_defs.h:
+/usr/local/src/esp/esp-idf/components/soc/esp32/interrupts.c:
